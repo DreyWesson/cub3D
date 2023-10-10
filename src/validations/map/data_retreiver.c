f89319a0fbc3043bf2baf5b6554fb42b753c7473
dreywesson <dreywesson@gmail.com>
2023-10-10 08:32:41 +0200

    add_texture function implemented

--- src/validations/map/data_retreiver.c
+++ src/validations/map/data_retreiver.c
@@ -6,11 +6,56 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 17:22:39 by doduwole          #+#    #+#             */
-/*   Updated: 2023/10/09 22:18:33 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/10/09 23:27:52 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/cub3d.h"
+
+static char	*add_tex_path(char *line, int j)
+{
+	int		len;
+	int		i;
+	char	*path;
+
+	while (line[j] && ft_isspace(line[j]))
+		j++;
+	len = j;
+	while (line[len] && !ft_isspace(line[j]))
+		len++;
+	path = malloc(sizeof(char) * (len - j + 1));
+	if (!path)
+		return (NULL);
+	i = 0;
+	while (line[j] && (!ft_isspace(line[j]) && line[j] != '\n'))
+		path[i++] = line[j++];
+	path[i] = '\0';
+	while (line[j] && (ft_isspace(line[j])))
+		j++;
+	if (line[j] && line[j] != '\n')
+	{
+		free(path);
+		path = NULL;
+	}
+	return (path);
+}
+
+static int	set_textures(t_dt *data, char *line, int j)
+{
+	if (line[j + 2] && ft_isprint_no_space(line[j + 2]))
+		return (ft_error("Textures: Invalid"), FAILURE);
+	if (line[j] == 'N' && line[j + 1] == 'O' && !(data->tex_north))
+		data->tex_north = add_tex_path(line, j + 2);
+	else if (line[j] == 'S' && line[j + 1] == 'O' && !(data->tex_south))
+		data->tex_south = add_tex_path(line, j + 2);
+	else if (line[j] == 'W' && line[j + 1] == 'E' && !(data->tex_west))
+		data->tex_west = add_tex_path(line, j + 2);
+	else if (line[j] == 'E' && line[j + 1] == 'A' && !(data->tex_east))
+		data->tex_east = add_tex_path(line, j + 2);
+	else
+		return (ft_error("Textures: Invalid"), FAILURE);
+	return (SUCCESS);
+}
 
 static int	skip_spaces(t_dt *data, char **cub_file, int i, int j)
 {
@@ -23,8 +68,8 @@ static int	skip_spaces(t_dt *data, char **cub_file, int i, int j)
 		{
 			if (DEBUG)
 				printf("%c %c\n", cub_file[i][j], cub_file[i][j + 1]);
-			// if (add_textures(data, cub_file[i], j) == FAILURE)
-			// 	return (FAILURE);
+			if (set_textures(data, cub_file[i], j) == FAILURE)
+				return (FAILURE);
 			return (TEXTURE_ADDED);
 		}
 		else
