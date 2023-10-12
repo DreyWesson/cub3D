61a4887649279956b4a73d18522fdd77095a9147
dreywesson <dreywesson@gmail.com>
2023-10-12 16:01:08 +0200

    applying stash to test

--- src/parse/index.c
+++ src/parse/index.c
@@ -6,7 +6,7 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 14:15:02 by doduwole          #+#    #+#             */
-/*   Updated: 2023/10/12 15:56:28 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/10/12 16:00:03 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int	parsing(t_dt *data, char **argv)
 		return (FAILURE);
 	if (DEBUG)
 		debugger(data);
-	if (validate_map(data, data->map) == FAILURE)
-		return (FAILURE);
+	// if (validate_map(data, data->map) == FAILURE)
+	// 	return (FAILURE);
 	if (validate_textures(data) == FAILURE)
 		return (FAILURE);
 	// add_player_direction(data);
