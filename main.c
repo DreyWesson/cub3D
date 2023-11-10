5b6b7de9dedde7d2c288fe25e03979eeb64518ee
Dare Oduwole <doduwole@c2s9d40.42wolfsburg.de>
2023-11-10 23:03:30 +0100

    removed comment

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loandrad <loandrad@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: doduwole <doduwole@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 13:27:01 by doduwole          #+#    #+#             */
-/*   Updated: 2023/11/09 13:04:33 by loandrad         ###   ########.fr       */
+/*   Updated: 2023/11/10 23:02:49 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ int	main(int argc, char **argv)
 	ft_memset((void *)&data, 0, sizeof(data));
 	if (parsing(&data, argv) == FAILURE)
 		return (free_data(&data), FAILURE);
-	// debugger(&data);
 	build_graphics(&data);
 	mlx_hook(data.mlx_win, 17, 0, end_program, &data);
 	mlx_hook(data.mlx_win, 2, 1L << 0, key_press_handler, &data);
