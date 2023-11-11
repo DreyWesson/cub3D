b83646ced84d301d2f8353f6b4691630e303d743
Dare Oduwole <doduwole@c2s9d40.42wolfsburg.de>
2023-11-11 22:22:35 +0100

    refactored game_init

--- src/graphics/init/game_init.c
+++ src/graphics/init/game_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   game_init.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loandrad <loandrad@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: doduwole <doduwole@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 01:00:58 by doduwole          #+#    #+#             */
-/*   Updated: 2023/11/09 12:43:43 by loandrad         ###   ########.fr       */
+/*   Updated: 2023/11/11 22:22:10 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,10 @@ int	game_init(t_data *data)
 {
 	data->mlx_ptr = mlx_init();
 	if (!data->mlx_ptr)
-	{
-		ft_error("MLX failed to initialize!");
-		return (FAILURE);
-	}
+		return (ft_error("MLX failed to initialize!"), FAILURE);
 	data->mlx_win = mlx_new_window(data->mlx_ptr,
 			data->win_width, data->win_height, "Cub3D");
 	if (!data->mlx_win)
-	{
-		ft_error("MLX failed to initialize!");
-		return (FAILURE);
-	}
+		return (ft_error("MLX failed to initialize!"), FAILURE);
 	return (SUCCESS);
 }
